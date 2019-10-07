8eb47b97c46a2d0319b08ca42739e806f4826ddd
valenovi76 <valentina.novara@bt.com>
2019-10-07 21:34:48 +0000

    WIP bar chart

--- static/js/graphs.js
+++ static/js/graphs.js
@@ -1,7 +1,77 @@
 queue()
-    .defer(d3.csv,"data/orders.csv")
+    .defer(d3.csv, "data/orders.csv")
     .await(makeGraphs);
-    
-function makeGraphs(error,ordersData){
-    
+
+function makeGraphs(error, orderData) {
+    var ndx = crossfilter(orderData);
+
+    show_order_wip(ndx);
+
+    dc.renderAll();
 }
+
+function show_order_wip(ndx) {
+    var dim = ndx.dimension(dc.pluck('Age_Status'));
+    var group = dim.group();
+
+    dc.barChart("#wip")
+        .width(400)
+        .height(300)
+        .margins({top: 10, right: 50, bottom: 30, left: 50})
+        .dimension(dim)
+        .group(group)
+        .transitionDuration(500)
+        .x(d3.scale.ordinal())
+        .xUnits(dc.units.ordinal)
+        .elasticY(true)
+        .xAxisLabel("Age_Status")
+        .yAxis().ticks(20);
+}
+
+
+
+// set the dimensions and margins of the graph
+var width = 450
+    height = 450
+    margin = 40
+
+// The radius of the pieplot is half the width or half the height (smallest one). I subtract a bit of margin.
+var radius = Math.min(width, height) / 2 - margin
+
+// append the svg object to the div called 'my_dataviz'
+var svg = d3.select("#pie")
+  .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+  .append("g")
+    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+// Create dummy data
+var data = {a: 9, b: 20, c:30, d:8, e:12}
+
+// set the color scale
+var color = d3.scalePoint()
+  .domain(data)
+  .range([0, 600]);
+
+// Compute the position of each group on the pie:
+var pie = d3.pie()
+  .value(function(d) {return d.value; })
+var data_ready = pie(d3.entries(data))
+
+// Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
+svg
+  .selectAll('whatever')
+  .data(data_ready)
+  .enter()
+  .append('path')
+  .attr('d', d3.arc()
+    .innerRadius(100)         // This is the size of the donut hole
+    .outerRadius(radius)
+  )
+  .attr('fill', function(d){ return(color(d.data.key)) })
+  .attr("stroke", "black")
+  .style("stroke-width", "2px")
+  .style("opacity", 0.7)
+
+
