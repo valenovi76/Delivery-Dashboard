f0b0f827f0fa72702a7023fbe93a2882e97da934
valenovi76 <valentina.novara@bt.com>
2019-10-10 15:26:30 +0000

    selector

--- static/js/graphs.js
+++ static/js/graphs.js
@@ -1,30 +1,41 @@
 queue()
     .defer(d3.csv, "data/inprogress_orders.csv")
-    .defer(d3.csv, "data/completed_orders.csv")
+
     .await(makeGraphs);
 
 function makeGraphs(error, orderData) {
     var ndx = crossfilter(orderData);
 
+   show_wip_group_selector(ndx);
     show_order_wip(ndx);
 
     dc.renderAll();
 }
 
-function show_order_wip(ndx) {
-    var dim = ndx.dimension(dc.pluck('Age_Status'));
+function show_wip_group_selector(ndx){
+    var dim = ndx.dimension(dc.pluck('Group'));
     var group = dim.group();
 
-    dc.barChart("#wip")
-        .width(400)
-        .height(300)
-        .margins({top: 10, right: 50, bottom: 30, left: 50})
-        .dimension(dim)
-        .group(group)
-        .transitionDuration(500)
-        .x(d3.scale.ordinal())
-        .xUnits(dc.units.ordinal)
-        .elasticY(true)
-        .xAxisLabel("Age_Status")
-        .yAxis().ticks(20);
+    dc.selectMenu("#wip_group_selector")
+       .dimesion(dim)
+        .group(group);
+
 }
+
+//function show_order_wip(ndx) {
+    //var dim = ndx.dimension(dc.pluck('Age_Status'));
+    //var group = dim.group();
+
+    //dc.barChart("#wip")
+      //  .width(400)
+        //.height(300)
+        //.margins({top: 10, right: 50, bottom: 30, left: 50})
+        //.dimension(dim)
+        //.group(group)
+        //.transitionDuration(500)
+        //.x(d3.scale.ordinal())
+        //.xUnits(dc.units.ordinal)
+        //.elasticY(true)
+        //.xAxisLabel("Age_Status")
+        //.yAxis().ticks(20);
+//}
