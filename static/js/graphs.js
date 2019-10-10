cbac3f5cc3922a19f7cba4023aa17be69fb00c75
valenovi76 <valentina.novara@bt.com>
2019-10-10 21:39:12 +0000

    pies

--- static/js/graphs.js
+++ static/js/graphs.js
@@ -1,17 +1,22 @@
+//load data
 queue()
-    .defer(d3.csv, "data/inprogress_orders.csv")
+    .defer(d3.csv, "data/orders.csv")
+
 
     .await(makeGraphs);
 
+// Make graphs function
 function makeGraphs(error, orderData) {
     var ndx = crossfilter(orderData);
 
    show_wip_group_selector(ndx);
     show_order_wip(ndx);
+show_order_type(ndx);
 
     dc.renderAll();
 }
 
+//Selector js
 function show_wip_group_selector(ndx){
     var dim = ndx.dimension(dc.pluck('Order_Type'));
     var group = dim.group();
@@ -23,6 +28,7 @@ function show_wip_group_selector(ndx){
 
 }
 
+//WIP js
 function show_order_wip(ndx) {
     var dim = ndx.dimension(dc.pluck('Age_Status'));
     var group = dim.group();
@@ -39,4 +45,19 @@ function show_order_wip(ndx) {
         .elasticY(true)
         .xAxisLabel("Age_Status")
         .yAxis().ticks(20);
+}
+
+//Order Type js
+function show_order_type(ndx) {
+    var dim = ndx.dimension(dc.pluck('Order_Type'));
+    var group = dim.group();
+
+    dc.pieChart("#order_type")
+        .width(400)
+        .height(300)
+        .slicesCap(4)
+        .innerRadius(100)
+        .dimension(dim)
+        .group(group)
+        .legend(dc.legend())
 }
