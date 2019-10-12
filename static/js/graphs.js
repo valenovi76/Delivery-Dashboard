617496f396d5077510c7ab5a864022e1481cd5ad
valenovi76 <valentina.novara@bt.com>
2019-10-12 08:36:38 +0000

    nesting completed

--- static/js/graphs.js
+++ static/js/graphs.js
@@ -1,17 +1,16 @@
 //load data
 queue()
     .defer(d3.csv, "data/orders.csv")
-
-
     .await(makeGraphs);
 
 // Make graphs function
 function makeGraphs(error, orderData) {
     var ndx = crossfilter(orderData);
 
-   show_wip_group_selector(ndx);
+    show_wip_group_selector(ndx);
     show_order_wip(ndx);
-show_order_type(ndx);
+    show_order_type(ndx);
+    show_onTime_late(ndx)
 
     dc.renderAll();
 }
@@ -61,76 +60,25 @@ function show_order_type(ndx) {
         .group(group)
         .legend(dc.legend())
 }
-//line and bar
- var svg = d3.select("body").append("svg")
-      .attr("width", 960)
-      .attr("height", 500);
+//onTime-late js
+function show_onTime_late(ndx) {
+    var completedonly = d3.nest()
+    .key(function(d) { return d.Order_Status; })
+    .entries(Completed);
+    var dim = ndx.dimension(dc.pluck('On_Time'));
+    var group = dim.group();
 
-    function generate_data() {
-      return d3.range(10).map(function() {
-        return Math.random();
-      });
-    }
+    dc.barChart("#onTime_late")
+        .width(400)
+        .height(300)
+        .margins({top: 10, right: 50, bottom: 30, left: 50})
+        .dimension(dim)
+        .group(group)
+        .transitionDuration(500)
+        .x(d3.scale.ordinal())
+        .xUnits(dc.units.ordinal)
+        .elasticY(true)
+        .xAxisLabel("Age_Status")
+        .yAxis().ticks(20);
+}
 
-    var data = generate_data();
-
-    var y = d3.scaleLinear()
-    	.domain([0, d3.max(data)])
-    	.range([0, +d3.select("svg").attr("height")/3]);
-
-    var r = d3.scaleLinear()
-    	.domain([0, d3.max(data)])
-    	.range([2, 10]);
-
-    // ordinal
-    var x = d3.scaleBand()
-    	.domain(d3.range(10))
-    	.range([0, +d3.select("svg").attr("width")]);
-
-		var s = svg.selectAll("rect").data(data);
-
-    s.enter()
-     .append("rect")
-
-     svg.selectAll("rect").data(data)
-	    .attr("width", function(d, i) { return x.bandwidth(); })
-      .attr("height", function(d, i) { return y(d); })
-      .attr("x", function(d, i) { return x(i); })
-      .attr("y",  function(d) { return 200 - y(d); })
-
-    svg.selectAll("rect")
-	    .attr("width", function(d, i) { return x.bandwidth(); })
-      .attr("height", function(d, i) { return y(d); })
-     	.on("mouseenter", function(d) {
-      	if(typeof d.__clicked === "undefined" || d.__clicked !== true) {
-	      	d3.select(this).style("fill", "red")
-        }
-    	})
-    	.on("mouseleave", function(d) {
-      	if(typeof d.__clicked === "undefined" || d.__clicked !== true) {
-	      	d3.select(this).style("fill", "white")
-        }
-    	})
-       .on("click", function(d) {
-      	d3.select(this).transition().duration(1000).style("fill", "green")
-        d.__clicked = true;
-    	});
-
-		var s = svg.selectAll("circle").data(data);
-
-    s.enter()
-     	.append("circle")
-
-     svg.selectAll("circle").data(data)
-      .attr("cx", function(d, i) { return x(i) + x.bandwidth() / 2; })
-      .attr("cy", function(d) { return 200 - y(d); })
-     	.attr("r", function(d) { return r(d); });
-
-    var line = d3.line()
-     .x(function(d, i) { return x(i) + x.bandwidth() /2 ; })
-     .y(function(d) { return 200 - y(d); });
-
-
-		var s = svg.selectAll("path").data([data]).enter()
-    	.append("path")
-    	.attr("d", function(d) { return line(d); })
