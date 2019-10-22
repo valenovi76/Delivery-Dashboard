095b0b1de331e9a1cf411129fd4cf5176f77bbe2
valenovi76 <valentina.novara@bt.com>
2019-10-22 21:50:54 +0000

    second selector

--- static/js/bundle.js
+++
@@ -1,8 +0,0 @@
-var chart = c3.generate({
-     bindto: '#chart',
-    data: {
-        x: "Age_Status",
-        url: '/data/created_completed.csv'
-    },
-    type:"pie"
-});
--- static/js/graphs.js
+++ static/js/graphs.js
@@ -14,7 +14,8 @@ function makeGraphs(error, orderData) {
     show_wip_group_selector(ndx);
     show_order_wip(ndx);
     show_order_type(ndx);
-    show_created_completed(ndx);
+    show_created(ndx);
+    show_completed(ndx);
 
     dc.renderAll();
 }
@@ -22,11 +23,17 @@ function makeGraphs(error, orderData) {
 
 //Selector js
 function show_wip_group_selector(ndx){
-    var dim = ndx.dimension(dc.pluck('Order_Type'));
+    var dim = ndx.dimension(dc.pluck('Channel'));
     var group = dim.group();
-
-
     var select = dc.selectMenu("#wip_group_selector")
+        .dimension(dim)
+        .group(group);
+
+}
+function show_product_selector(ndx){
+    var dim = ndx.dimension(dc.pluck('Product'));
+    var group = dim.group();
+    var select = dc.selectMenu("#product_selector")
         .dimension(dim)
         .group(group);
 
@@ -66,92 +73,39 @@ function show_order_type(ndx) {
         .legend(dc.legend())
 }
 //created-completed js
-//function show_created_completed(ndx) {
-//var dim = ndx.dimension(dc.pluck('Age_Status'));
-    //var group = dim.group();
+function show_created(ndx) {
+var dim = ndx.dimension(dc.pluck('Created_Month'));
+var group = dim.group();
 
-   // dc.lineChart("#bar_line")
-       // .width(400)
-       // .height(300)
-        //.margins({top: 10, right: 50, bottom: 30, left: 50})
-      //  .dimension(dim)
-      //  .group(group)
-      //  .transitionDuration(500)
-      //  .x(d3.scale.ordinal())
-      //  .xUnits(dc.units.ordinal)
-      //  .elasticY(true)
-      //  .xAxisLabel("Age_Status")
-      //  .yAxis().ticks(20);
- //}
+dc.lineChart("#created_line")
+.width(400)
+.height(300)
+.margins({top: 10, right: 50, bottom: 30, left: 50})
+.dimension(dim)
+.group(group)
+.transitionDuration(500)
+.x(d3.scale.ordinal())
+.xUnits(dc.units.ordinal)
+.elasticY(true)
+.xAxisLabel("Age_Status")
+.yAxis().ticks(20);
+ }
 
+//completed line
+function show_completed(ndx) {
+var dim = ndx.dimension(dc.pluck('Created_Month'));
+var group = dim.group();
 
-function show_created_completed(ndx){
-// set the dimensions and margins of the graph
-var margin = {top: 10, right: 30, bottom: 20, left: 50},
-    width = 460 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
-
-// append the svg object to the body of the page
-var svg = d3.select("#my_dataviz")
-  .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform",
-          "translate(" + margin.left + "," + margin.top + ")");
-
-// Parse the Data
-
-//d3.csv("data/orders.csv", function(data) {
-  // List of subgroups = header of the csv files = soil condition here
-
-  var subgroups = ndx.columns.slice(1)
-
-  // List of groups = species here = value of the first column called group -> I show them on the X axis
-  var groups = d3.map(orderData, function(d){return(d.group)}).keys()
-
-  // Add X axis
-  var x = d3.scaleBand()
-      .domain(groups)
-      .range([0, width])
-      .padding([0.2])
-  svg.append("g")
-    .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(x).tickSize(0));
-
-  // Add Y axis
-  var y = d3.scaleLinear()
-    .domain([0, 40])
-    .range([ height, 0 ]);
-  svg.append("g")
-    .call(d3.axisLeft(y));
-
-  // Another scale for subgroup position?
-  var xSubgroup = d3.scaleBand()
-    .domain(subgroups)
-    .range([0, x.bandwidth()])
-    .padding([0.05])
-
-  // color palette = one color per subgroup
-  var color = d3.scaleOrdinal()
-    .domain(subgroups)
-    .range(['#e41a1c','#377eb8','#4daf4a'])
-
-  // Show the bars
-  svg.append("g")
-    .selectAll("g")
-    // Enter in data = loop group per group
-    .data(ndx)
-    .enter()
-    .append("g")
-      .attr("transform", function(d) { return "translate(" + x(d.group) + ",0)"; })
-    .selectAll("rect")
-    .data(function(d) { return subgroups.map(function(key) { return {key: key, value: d[key]}; }); })
-    .enter().append("rect")
-      .attr("x", function(d) { return xSubgroup(d.key); })
-      .attr("y", function(d) { return y(d.value); })
-      .attr("width", xSubgroup.bandwidth())
-      .attr("height", function(d) { return height - y(d.value); })
-      .attr("fill", function(d) { return color(d.key); });
-
-}
+dc.lineChart("#created_line")
+.width(400)
+.height(300)
+.margins({top: 10, right: 50, bottom: 30, left: 50})
+.dimension(dim)
+.group(group)
+.transitionDuration(500)
+.x(d3.scale.ordinal())
+.xUnits(dc.units.ordinal)
+.elasticY(true)
+.xAxisLabel("Age_Status")
+.yAxis().ticks(20);
+ }
