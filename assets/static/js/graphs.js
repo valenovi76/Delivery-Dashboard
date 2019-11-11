10845f900492536f83b86878942c83720b2f754b
valenovi76 <valentina.novara@bt.com>
2019-11-11 13:53:58 +0000

    beautified and checked on w3 all codes

--- assets/static/js/graphs.js
+++ assets/static/js/graphs.js
@@ -1,8 +1,8 @@
 //load data
 queue()
-.defer(d3.csv, "/assets/data/orders.csv")
+    .defer(d3.csv, "/assets/data/orders.csv")
 
-.await(makeGraphs);
+    .await(makeGraphs);
 
 
 // Make graphs function
@@ -11,27 +11,25 @@ function makeGraphs(error, orderData) {
     var ndx = crossfilter(orderData);
 
     var parseDate = d3.time.format("%d/%m/%Y").parse;
-    orderData.forEach(function(d){
-    d.Completed_Month = parseDate(d.Completed_Month);
+    orderData.forEach(function(d) {
+        d.Completed_Month = parseDate(d.Completed_Month);
     });
 
 
 
-orderData.forEach(function(d){
-d.CT_Ex_Delayed_Days=parseInt(d.CT_Ex_Delayed_Days)})
+    orderData.forEach(function(d) {
+        d.CT_Ex_Delayed_Days = parseInt(d.CT_Ex_Delayed_Days)
+    })
 
 
 
 
-
-
-//var parseDate2 = d3.time.format("%B").parse;
-  //  orderData.forEach(function(d){
+    //var parseDate2 = d3.time.format("%B").parse;
+    //  orderData.forEach(function(d){
     //    d.Created_Month = parseDate2(d.Created_Month);
     //});
 
 
- 
 
 
     show_wip_group_selector(ndx);
@@ -56,7 +54,7 @@ d.CT_Ex_Delayed_Days=parseInt(d.CT_Ex_Delayed_Days)})
 
 
 //Selector js
-function show_wip_group_selector(ndx){
+function show_wip_group_selector(ndx) {
     var dim = ndx.dimension(dc.pluck('Channel'));
     var group = dim.group();
     var select = dc.selectMenu("#wip_group_selector")
@@ -64,7 +62,8 @@ function show_wip_group_selector(ndx){
         .group(group);
 
 }
-function show_product_selector(ndx){
+
+function show_product_selector(ndx) {
     var dim = ndx.dimension(dc.pluck('Product'));
     var group = dim.group();
     var select = dc.selectMenu("#product_selector")
@@ -72,9 +71,10 @@ function show_product_selector(ndx){
         .group(group);
 
 }
-function show_project_selector(ndx){
-    var dim=ndx.dimension(dc.pluck('Project'));
-    var group= dim.group();
+
+function show_project_selector(ndx) {
+    var dim = ndx.dimension(dc.pluck('Project'));
+    var group = dim.group();
     var select = dc.selectMenu("#project_selector")
         .dimension(dim)
         .group(group);
@@ -85,30 +85,33 @@ function show_percent_RFT_OnTime(ndx) {
     var percentageThatAreOnTime = ndx.groupAll().reduce(
         function(p, v) {
             p.total++;
-                if(v.On_Time === "On-Time") {
-                    p.match++;
-                }
-                return p;
-            },
-            function (p, v) {
-                p.total--;
-                if(v.On_Time === "On-Time") {
-                    p.match--;
-                }
-                return p;
-            },
-            function () {
-                return {total: 0, match: 0};
+            if (v.On_Time === "On-Time") {
+                p.match++;
             }
+            return p;
+        },
+        function(p, v) {
+            p.total--;
+            if (v.On_Time === "On-Time") {
+                p.match--;
+            }
+            return p;
+        },
+        function() {
+            return {
+                total: 0,
+                match: 0
+            };
+        }
     );
 
     dc.numberDisplay("#RFTPerc")
         .formatNumber(d3.format(".1%"))
-        .valueAccessor(function (d) {
+        .valueAccessor(function(d) {
             if (d.count == 0) {
                 return 0;
             } else {
-                return ( d.match/d.total );
+                return (d.match / d.total);
             }
         })
         .group(percentageThatAreOnTime)
@@ -118,110 +121,126 @@ function show_percent_CT_Pass(ndx) {
     var percentageThatArePassed = ndx.groupAll().reduce(
         function(p, v) {
             p.total++;
-                if(v.SLT_Status === "Pass") {
-                    p.match++;
-                }
-                return p;
-            },
-            function (p, v) {
-                p.total--;
-                if(v.SLT_Status === "Pass") {
-                    p.match--;
-                }
-                return p;
-            },
-            function () {
-                return {total: 0, match: 0};
+            if (v.SLT_Status === "Pass") {
+                p.match++;
             }
+            return p;
+        },
+        function(p, v) {
+            p.total--;
+            if (v.SLT_Status === "Pass") {
+                p.match--;
+            }
+            return p;
+        },
+        function() {
+            return {
+                total: 0,
+                match: 0
+            };
+        }
     );
 
     dc.numberDisplay("#CTPerc")
         .formatNumber(d3.format(".1%"))
-        .valueAccessor(function (d) {
+        .valueAccessor(function(d) {
             if (d.count == 0) {
                 return 0;
             } else {
-                return ( d.match/d.total );
+                return (d.match / d.total);
             }
         })
         .group(percentageThatArePassed)
 }
 //KPIS values - Monthly deliveries
-function show_monthly_delivery(ndx){
+function show_monthly_delivery(ndx) {
     var avgMonthlyDeliveries = ndx.groupAll().reduce(
         function(p, v) {
             p.total++;
-                if(v.Order_Status === "Completed") {
-                    p.match++;
-                }
-                return p;
-            },
-            function (p, v) {
-                p.total--;
-                if(v.Order_Status === "Completed") {
-                    p.match--;
-                }
-                return p;
-            },
-            function () {
-                return {total: 0, match: 0};
+            if (v.Order_Status === "Completed") {
+                p.match++;
             }
+            return p;
+        },
+        function(p, v) {
+            p.total--;
+            if (v.Order_Status === "Completed") {
+                p.match--;
+            }
+            return p;
+        },
+        function() {
+            return {
+                total: 0,
+                match: 0
+            };
+        }
     );
 
     dc.numberDisplay("#avg_monthly_delivery")
         .formatNumber(d3.format(".1f"))
-        .valueAccessor(function (d) {
+        .valueAccessor(function(d) {
             if (d.total == 0) {
                 return 0;
             } else {
-                return ( d.match/6 );
+                return (d.match / 6);
             }
         })
         .group(avgMonthlyDeliveries)
 }
 //created line js
 function show_created(ndx) {
-var dim = ndx.dimension(dc.pluck('Created_Month'));
-console.log(dim);
-var group =dim.group();
+    var dim = ndx.dimension(dc.pluck('Created_Month'));
+    console.log(dim);
+    var group = dim.group();
 
-dc.lineChart("#created")
+    dc.lineChart("#created")
 
-.width(400)
-.height(350)
-.margins({top: 30, right: 40, bottom: 50, left: 50})
-.dimension(dim)
-.group(group)
-.transitionDuration(500)
-.x(d3.scale.ordinal())
-.xUnits(dc.units.ordinal)
-.elasticY(true)
-.xAxisLabel("FY 2019-2020 Month")
-.colors(['#FF00FF'])
-.yAxis().ticks(20);
+        .width(400)
+        .height(350)
+        .margins({
+            top: 30,
+            right: 40,
+            bottom: 50,
+            left: 50
+        })
+        .dimension(dim)
+        .group(group)
+        .transitionDuration(500)
+        .x(d3.scale.ordinal())
+        .xUnits(dc.units.ordinal)
+        .elasticY(true)
+        .xAxisLabel("FY 2019-2020 Month")
+        .colors(['#FF00FF'])
+        .yAxis().ticks(20);
 
- }
+}
 //completed line js
 function show_completed(ndx) {
 
-var dim = ndx.dimension(dc.pluck('Completed_Month'));
-var group = dim.group();
+    var dim = ndx.dimension(dc.pluck('Completed_Month'));
+    var group = dim.group();
 
-dc.lineChart("#completed")
+    dc.lineChart("#completed")
 
-.width(400)
-.height(350)
-.margins({top: 30, right: 40, bottom: 50, left: 50})
-.dimension(dim)
-.group(group)
-.transitionDuration(500)
-.x(d3.scale.ordinal())
-.xUnits(dc.units.ordinal)
-.elasticY(true)
-.xAxisLabel("FY 2019-2020 Month")
-.colors("#34DDDD")
-.yAxis().ticks(20);
- }
+        .width(400)
+        .height(350)
+        .margins({
+            top: 30,
+            right: 40,
+            bottom: 50,
+            left: 50
+        })
+        .dimension(dim)
+        .group(group)
+        .transitionDuration(500)
+        .x(d3.scale.ordinal())
+        .xUnits(dc.units.ordinal)
+        .elasticY(true)
+        .xAxisLabel("FY 2019-2020 Month")
+        .colors("#34DDDD")
+        .yAxis().ticks(20);
+}
 //WIP pie js
 function show_order_order_type(ndx) {
     var dim = ndx.dimension(dc.pluck('Order_Type'));
@@ -237,74 +256,91 @@ function show_order_order_type(ndx) {
         .innerRadius(60)
         .dimension(dim)
         .group(group)
-        .ordinalColors(["#750075","#D100D1","#FF2EFF","#FF8AFF"])
+        .ordinalColors(["#750075", "#D100D1", "#FF2EFF", "#FF8AFF"])
         .legend(dc.legend())
 
 }
 //CT Avg line js
-function show_ct_avg(ndx){
-var dim = ndx.dimension(dc.pluck("Completed_Month"));
-function add_item(p,v){
-p.count ++;
-p.total += v.CT_Ex_Delayed_Days;
-p.average = p.total/p.count;
-return p;
-}
-function remove_item(p,v){
-p.count--;
-if(p.count==0){
-p.total=0;
-p.average=0;}else{
-p.total -= v.CT_Ex_Delayed_Days;
-p.average = p.total/p.count;
-}
-return p;
-}
-function initialise(){
-    return{count:0, total:0, average:0};
-}
+function show_ct_avg(ndx) {
+    var dim = ndx.dimension(dc.pluck("Completed_Month"));
+
+    function add_item(p, v) {
+        p.count++;
+        p.total += v.CT_Ex_Delayed_Days;
+        p.average = p.total / p.count;
+        return p;
+    }
+
+    function remove_item(p, v) {
+        p.count--;
+        if (p.count == 0) {
+            p.total = 0;
+            p.average = 0;
+        } else {
+            p.total -= v.CT_Ex_Delayed_Days;
+            p.average = p.total / p.count;
+        }
+        return p;
+    }
+
+    function initialise() {
+        return {
+            count: 0,
+            total: 0,
+            average: 0
+        };
+    }
 
 
-var group= dim.group().reduce(add_item,remove_item,initialise);
+    var group = dim.group().reduce(add_item, remove_item, initialise);
 
 
-dc.lineChart("#ct_avg")
-.width(400)
-.height(350)
-.margins({top: 10, right: 50, bottom: 30, left: 50})
-.dimension(dim)
-.group(group)
-.valueAccessor(function(d){
-return d.value.average})
-.transitionDuration(500)
-.x(d3.scale.ordinal())
-.xUnits(dc.units.ordinal)
-.elasticY(true)
-.colors("#FF00FF")
-.xAxisLabel("FY 2019-2020 Month")
-.yAxis().ticks(20);
+    dc.lineChart("#ct_avg")
+        .width(400)
+        .height(350)
+        .margins({
+            top: 10,
+            right: 50,
+            bottom: 30,
+            left: 50
+        })
+        .dimension(dim)
+        .group(group)
+        .valueAccessor(function(d) {
+            return d.value.average
+        })
+        .transitionDuration(500)
+        .x(d3.scale.ordinal())
+        .xUnits(dc.units.ordinal)
+        .elasticY(true)
+        .colors("#FF00FF")
+        .xAxisLabel("FY 2019-2020 Month")
+        .yAxis().ticks(20);
 
 }
 //CT perf js
-function show_SLT_perf(ndx){
+function show_SLT_perf(ndx) {
     function SLTPerfByMonth(dimension, SLT_Status) {
         return dimension.group().reduce(
-            function (p, v) {
+            function(p, v) {
                 p.total++;
-                if(v.SLT_Status == SLT_Status) {
+                if (v.SLT_Status == SLT_Status) {
                     p.match++;
                 }
                 return p;
             },
-            function (p, v) {
+            function(p, v) {
                 p.total--;
-                if(v.SLT_Status == SLT_Status) {
+                if (v.SLT_Status == SLT_Status) {
                     p.match--;
                 }
                 return p;
             },
-            function () {
-                return {total: 0, match: 0};
+            function() {
+                return {
+                    total: 0,
+                    match: 0
+                };
             }
         );
     }
@@ -320,7 +356,7 @@ function show_SLT_perf(ndx){
         .group(PassByMonth, "Pass")
         .stack(FailByMonth, "Fail")
         .valueAccessor(function(d) {
-            if(d.value.total > 0) {
+            if (d.value.total > 0) {
                 return (d.value.match / d.value.total) * 100;
             } else {
                 return 0;
@@ -329,30 +365,38 @@ function show_SLT_perf(ndx){
         .x(d3.scale.ordinal())
         .xUnits(dc.units.ordinal)
         .legend(dc.legend().x(320).y(20).itemHeight(15).gap(5))
-        .ordinalColors(["#fc5603","#FF00FF"])
-        .margins({top: 10, right: 100, bottom: 30, left: 30});
-        //.ordinalColors(['#d93733','#108a24']);
+        .ordinalColors(["#fc5603", "#FF00FF"])
+        .margins({
+            top: 10,
+            right: 100,
+            bottom: 30,
+            left: 30
+        });
+    //.ordinalColors(['#d93733','#108a24']);
 }
 //On time vs Late stacked-bars graph
 function show_ontime_late(ndx) {
     function RFTPerfByMonth(dimension, On_Time) {
         return dimension.group().reduce(
-            function (p, v) {
+            function(p, v) {
                 p.total++;
-                if(v.On_Time == On_Time) {
+                if (v.On_Time == On_Time) {
                     p.match++;
                 }
                 return p;
             },
-            function (p, v) {
+            function(p, v) {
                 p.total--;
-                if(v.On_Time == On_Time) {
+                if (v.On_Time == On_Time) {
                     p.match--;
                 }
                 return p;
             },
-            function () {
-                return {total: 0, match: 0};
+            function() {
+                return {
+                    total: 0,
+                    match: 0
+                };
             }
         );
     }
@@ -368,7 +412,7 @@ function show_ontime_late(ndx) {
         .group(OnTimeByMonth, "On-Time")
         .stack(LateByMonth, "Late")
         .valueAccessor(function(d) {
-            if(d.value.total > 0) {
+            if (d.value.total > 0) {
                 return (d.value.match / d.value.total) * 100;
             } else {
                 return 0;
@@ -376,13 +420,18 @@ function show_ontime_late(ndx) {
         })
         .x(d3.scale.ordinal())
         .xUnits(dc.units.ordinal)
-        .ordinalColors(["#fc5603","#FF00FF"])
+        .ordinalColors(["#fc5603", "#FF00FF"])
         .legend(dc.legend().x(320).y(20).itemHeight(15).gap(5))
-        .margins({top: 10, right: 100, bottom: 30, left: 30});
-   }
+        .margins({
+            top: 10,
+            right: 100,
+            bottom: 30,
+            left: 30
+        });
+}
 //delivery_Product(ndx);
-function show_delivery_Product(ndx){
-var dim = ndx.dimension(dc.pluck('Product'));
+function show_delivery_Product(ndx) {
+    var dim = ndx.dimension(dc.pluck('Product'));
     var group = dim.group();
 
 
@@ -393,7 +442,7 @@ var dim = ndx.dimension(dc.pluck('Product'));
         .slicesCap(4)
         .radius(100)
         .innerRadius(60)
-        .ordinalColors(["#34DDDD","#FF0BAC","#6AF2F0","#54086B","#fc5603"])
+        .ordinalColors(["#34DDDD", "#FF0BAC", "#6AF2F0", "#54086B", "#fc5603"])
         .dimension(dim)
         .group(group)
         .legend(dc.legend())
@@ -403,22 +452,25 @@ var dim = ndx.dimension(dc.pluck('Product'));
 function show_delivery_Project(ndx) {
     function delbyproject(dimension, On_Time) {
         return dimension.group().reduce(
-            function (p, v) {
+            function(p, v) {
                 p.total++;
-                if(v.On_Time == On_Time) {
+                if (v.On_Time == On_Time) {
                     p.match++;
                 }
                 return p;
             },
-            function (p, v) {
+            function(p, v) {
                 p.total--;
-                if(v.On_Time == On_Time) {
+                if (v.On_Time == On_Time) {
                     p.match--;
                 }
                 return p;
             },
-            function () {
-                return {total: 0, match: 0};
+            function() {
+                return {
+                    total: 0,
+                    match: 0
+                };
             }
         );
     }
@@ -434,29 +486,36 @@ function show_delivery_Project(ndx) {
         .group(OnTimeByMonth, "On-Time")
         .stack(LateByMonth, "Late")
         .valueAccessor(function(d) {
-            if(d.value.total > 0) {
+            if (d.value.total > 0) {
                 return (d.value.match / d.value.total) * 100;
             } else {
                 return 0;
             }
         })
         .x(d3.scale.ordinal())
-        .ordinalColors(["#fc5603","#FF00FF"])
+        .ordinalColors(["#fc5603", "#FF00FF"])
         .xUnits(dc.units.ordinal)
         .legend(dc.legend().x(320).y(20).itemHeight(15).gap(5))
-        .margins({top: 10, right: 100, bottom: 30, left: 30});
-   }
+        .margins({
+            top: 10,
+            right: 100,
+            bottom: 30,
+            left: 30
+        });
+}
 //Delivery Country RFT composite
-   function show_delivery_country_composite(ndx){
-   var dateDim = ndx.dimension(function(d) {return d.Completed_Month;});
-   var CT = dateDim.group().reduceSum(dc.pluck('CT_Ex_Delayed_Days'));
+function show_delivery_country_composite(ndx) {
+    var dateDim = ndx.dimension(function(d) {
+        return d.Completed_Month;
+    });
+    var CT = dateDim.group().reduceSum(dc.pluck('CT_Ex_Delayed_Days'));
     var minDate = dateDim.bottom(1)[0].date;
     var maxDate = dateDim.top(1)[0].date;
-    var CTlineChart  = dc.lineChart("#del_country");
+    var CTlineChart = dc.lineChart("#del_country");
 
     CTlineChart
-	.width(400).height(350)
-	.dimension(dateDim)
-	.group(CT)
-	.x(d3.time.scale().domain([minDate,maxDate]));
-    }
+        .width(400).height(350)
+        .dimension(dateDim)
+        .group(CT)
+        .x(d3.time.scale().domain([minDate, maxDate]));
+}
